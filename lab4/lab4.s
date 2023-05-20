5b2f1120aa24391c10567c62e0f6616c01924d78
DeToNator666 <dima-steam008@mail.ru>
2023-05-20 11:40:59 -0400

    made beginning lab + right Makefile

--- lab4/lab4.s
+++ lab4/lab4.s
@@ -1,1 +1,18 @@
-abobus
+section .data
+    str_format db "%s", 0
+    input_msg db "Input x: ", 0
+
+section .text
+    extern scanf, printf
+    global main
+
+main:
+	; Ввод x
+	xor rax, rax
+	mov rsi, str_format
+    	mov rdi, input_msg
+    	call printf
+
+    	; Выход из программы
+    	xor eax, eax
+    	ret
