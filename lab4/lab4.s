de820b43abd1de025830d486c54d47e9de0d6b60
DeToNator666 <dima-steam008@mail.ru>
2023-05-21 05:33:00 -0400

    Рабочий ввод-вывод для float

--- lab4/lab4.s
+++ lab4/lab4.s
@@ -1,18 +1,33 @@
 section .data
-    str_format db "%s", 0
-    input_msg db "Input x: ", 0
+        str_format db "%s", 0
+        input_msg db "Input x: ", 0
+        float_format db "%f", 0
+        msg_res_float_format db "Result: %f", 0x0a,0
+        x dw 0.0
+global main
 
 section .text
     extern scanf, printf
-    global main
 
 main:
-	; Ввод x
-	xor rax, rax
-	mov rsi, str_format
-    	mov rdi, input_msg
-    	call printf
+        ; Ввод x
+        sub rsp, 8
+        xor rax, rax
+        mov rsi, str_format
+        mov rdi, input_msg
+        call printf
 
-    	; Выход из программы
-    	xor eax, eax
-    	ret
+        mov rax, 0
+        lea rdi, [float_format]
+        mov rsi, x
+        call scanf
+
+        cvtss2sd xmm0, [x]
+        mov rdi, msg_res_float_format
+        mov eax, 1
+        call printf
+
+        ; Выход из программы
+        xor eax, eax
+        add rsp, 8
+        ret
